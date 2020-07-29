9e5eab11713cd16e597ab0c474885fa09bc5f2bb
vinaybana <vinay@securitytroops.com>
2020-07-29 17:06:12 +0530

    all input updated

--- jqrs.js
+++ jqrs.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 	// });
 	$("#exampleModal .btn-primary").click( function(){
     	var valu = $('#heading-name').val();  											// get value of input of heading modal
-    	$('main').append('<section><h2>'+valu+'<button type="button">X</button></h2></section>');  						 // stores input of heading modal in h2 tag 
+    	$('main').append('<section><h2>'+valu+'<button type="button" value="submit">X</button></h2></section>');  						 // stores input of heading modal in h2 tag 
     	$(".sbhdng select option").remove();  					  						 // remove static values of option of select
     	$(".sbhdng select").append('<option value=""></option>'); 						 // create dynamic option for select
 		$(".frmhdg select option").remove();
@@ -14,7 +14,7 @@ $(document).ready(function(){
 		// console.log(len);
 		$('main section h2').each(function(key){
 			key=key+1	
-			var hdd=$(this).text();		
+			var hdd=$(this).text();	      	
 			$(".sbhdng select").append('<option value="'+key+'">'+hdd+'</option>');	   // dynamically value and text created for option of select
 			$(".frmhdg select").append('<option value="'+key+'">'+hdd+'</option>');	
 		});
@@ -40,7 +40,6 @@ $(document).ready(function(){
 			key = key+1
 			// console.log(zxc);
 			$('.sbhdg select').append('<option value="'+key+'">'+zxc+'</option>')
-	
 		});
 	}); 
 
@@ -57,42 +56,38 @@ $(document).ready(function(){
 		var cls = $('.clas').val();
 		var vlu = $('.vl').val();
 		if (fmin == 'checkbox'){
-			console.log("ift");
 			var lbls = lbl.split(',');
-			console.log(lbls);
 			$(lbls).each(function(key){	
 				$('main section:nth-child('+fsh+') div:nth-child('+fss+')').append('<label>'+lbls[key]+'</label><input type="'+fmin+'" value="'+vlu+'" name="'+nm+'">');
 			});
 		}
 		else if (fmin == 'radio'){
-			console.log("elif1");
 			var lbls = lbl.split(',');
-			console.log(lbls);
 			$(lbls).each(function(key){	
 				$('main section:nth-child('+fsh+') div:nth-child('+fss+')').append('<label>'+lbls[key]+'</label><input type="'+fmin+'" value="'+vlu+'" name="'+nm+'">');
 			});
 		}
 		else if  (fmin == 'select'){
-			console.log("elif2");
 			var vlus = vlu.split(',');
-			console.log(vlus);
 			var p = $('<select><option>select</option></select>');
 			for (i=0; i< vlus.length; i++){
 				var p = $(p).append('<option value="'+vlus[i]+'">'+vlus[i]+'</option>')
-				console.log(p);	
 			}
-			$('main section:nth-child('+fsh+') div:nth-child('+fss+')').append(p);
-			
+			$('main section:nth-child('+fsh+') div:nth-child('+fss+')').append(p);			
+		}
+		else if (fmin = 'textarea'){
+			$('main section:nth-child('+fsh+') div:nth-child('+fss+')').append('<label>'+lbl+'</label><input name="'+nm+'" rows="4" cols="50" value="'+vlu+'">');
 		}
 		else{	
-			console.log("el");
 			$('main section:nth-child('+fsh+') div:nth-child('+fss+')').append('<label>'+lbl+'</label><input type="'+fmin+'" name="'+nm+'" class="'+cls+'" value="'+vlu+'">');			// to append main>section>div to store input values
 		}
 		$('#exampleModal2').modal('hide');
-		
-
 	});
 });
+
+$('main .button').click(function(){
+	console.log("heyyyyyyyyy");
+})
 
 
 	
